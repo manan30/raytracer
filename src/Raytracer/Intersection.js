545aaa384799f2e1c1189d616a0625bfd9cdc169
Manan Joshi <mananjoshi1995@gmail.com>
2020-04-16 17:43:36 -0400

    Refactoring intersection class

--- src/Raytracer/Intersection.js
+++ src/Raytracer/Intersection.js
@@ -1,7 +1,10 @@
 export default class Intersection {
-  constructor(object, ray, dist) {
-    this.object = object;
-    this.ray = ray;
-    this.dist = dist;
+  constructor(isHit, position, normal, ray, material) {
+    this.isHit = isHit;
+    this.position = position;
+    this.normal = normal;
+    this.material = material;
+    if (this.isHit) this.distance = position.distance(ray.origin);
+    else this.distance = Number.MAX_SAFE_INTEGER;
   }
 }
