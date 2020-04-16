def5256d4f0826e20c432f5fd198c97de80f3a7f
Manan Joshi <mananjoshi1995@gmail.com>
2020-04-16 17:48:11 -0400

    Refactoring Light class

--- src/Raytracer/Light.js
+++ src/Raytracer/Light.js
@@ -1,6 +1,23 @@
+import Vector from './Vector';
+
 export default class Light {
   constructor(position, color) {
     this.position = position;
     this.color = color;
+    this.attenuation = new Vector(0, 0, 1);
+  }
+
+  distance(point) {
+    this.position.distance(point);
+  }
+
+  intensityAt(point) {
+    const distance = this.distance(point);
+    const intensity = this.power.scalarDivide(
+      this.attenuation.x +
+        this.attenuation.y * distance +
+        this.attenuation.z * distance * distance
+    );
+    return intensity;
   }
 }
